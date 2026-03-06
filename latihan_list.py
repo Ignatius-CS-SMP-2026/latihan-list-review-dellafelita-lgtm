ec46941571902bcb51bbe315979d759602f78530
dellafelita-lgtm <dellafelitaokey@gmail.com>
2026-03-06 20:31:37 +0700

    selesai

--- latihan_list.py
+++ latihan_list.py
@@ -1,18 +1,34 @@
-# NAMA  : 
-# KELAS : 
+# NAMA  : DELLA FELITA
+# KELAS : 9B
 # ---------------------------------------------------------
 # LATIHAN: REVIEW LIST PYTHON
-#Diberikan sebuah data acak nilai ujian siswa. Buatlah program yang mengurutkan data tersebut 
-#dari nilai tertinggi ke terendah. Kemudian, pisahkan 3 nilai tertinggi sebagai penerima beasiswa, 
-#dan hapus nilai terendah (nilai di bawah 60) karena dianggap tidak lulus. 
-#Gunakan data awal berikut: nilai_ujian = [75, 55, 90, 85, 45, 95, 80] 
+# Diberikan sebuah data acak nilai ujian siswa. Buatlah program yang mengurutkan data tersebut 
+# dari nilai tertinggi ke terendah. Kemudian, pisahkan 3 nilai tertinggi sebagai penerima beasiswa, 
+# dan hapus nilai terendah (nilai di bawah 60) karena dianggap tidak lulus. 
+# Gunakan data awal berikut: nilai_ujian = [75, 55, 90, 85, 45, 95, 80] 
 # ---------------------------------------------------------
 
 # OUTPUT
-#Data nilai asli: [75, 55, 90, 85, 45, 95, 80] 
-#Data setelah diurutkan (Descending): [95, 90, 85, 80, 75, 55, 45] 
-#Tiga nilai tertinggi (Penerima Beasiswa): [95, 90, 85] 
-#Daftar nilai yang lulus: [95, 90, 85, 80, 75]
+# Data nilai asli: [75, 55, 90, 85, 45, 95, 80] 
+# Data setelah diurutkan (Descending): [95, 90, 85, 80, 75, 55, 45] 
+# Tiga nilai tertinggi (Penerima Beasiswa): [95, 90, 85] 
+# Daftar nilai yang lulus: [95, 90, 85, 80, 75]
 # ---------------------------------------------------------
 
 # Tulis kodemu di bawah ini:
+nilai_ujian = [75, 55, 90, 85, 45, 95, 80]
+
+print("Data nilai asli:", nilai_ujian)
+
+nilai_ujian.sort(reverse=True)
+print("Data setelah diurutkan (Descending):", nilai_ujian)
+
+beasiswa = nilai_ujian[:3]
+print("Tiga nilai tertinggi (Penerima Beasiswa):", beasiswa)
+
+lulus = []
+for nilai in nilai_ujian:
+    if nilai >= 60:
+        lulus.append(nilai)
+
+print("Daftar nilai yang lulus:", lulus)
